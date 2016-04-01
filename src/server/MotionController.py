635a3b12e10d6e4d65d27d39ae8d5b01637f9edf
Stetson Gafford <stetsongafford@gmail.com>
2016-04-01 10:02:31 -0400

    Updates to MotionController

--- src/server/MotionController.py
+++ src/server/MotionController.py
@@ -4,14 +4,14 @@ import time
 
 class MotionController() :
 
-	move_stiffness = 1
-	rest_stiffness = 0
-	joint_speed = 0.1
-
 	def __init__(self):
 		self.robot = ALProxy("ALMotion", "lain.local", 9559)
+		self.move_stiffness = 1
+		self.rest_stiffness = 0
+		self.joint_speed = 0.1
 
-	### Sends the command to change a joint angle on the NAO
+	### Sends the command to change a joint angle on the NAO relative to
+	# the exisitng joint angle
 	# Sets stiffness to move_stiffness during motion and then
 	# returns to rest_stiffness when complete
 	def changeJointAngle(self, jointName, angle):
@@ -19,15 +19,38 @@ class MotionController() :
 		self.robot.changeAngles(jointName, angle, self.joint_speed)
 		time.sleep(1)
 		self.robot.setStiffnesses(jointName, self.rest_stiffness)
+		return True
+
+	### Sends the command to set anj absolute joint angle on the NAO
+	# Sets stiffness to move_stiffness during motion and then
+	# returns to rest_stiffness when complete
+	def setJointAngle(self, jointName, angle):
+		self.robot.setStiffnesses(jointName, self.move_stiffness)
+		self.robot.setAngles(jointName, angle, self.joint_speed)
+		time.sleep(1)
+		self.robot.setStiffnesses(jointName, self.rest_stiffness)
+		return True
+
+
+	def getJointAngles(self, jointNames):
+		useSensors = True
+		return self.robot.getAngles(jointNames, useSensors)
+
 
 	def setMoveStiffness(self, stiffness):
 		if stiffness >= 0 and stiffness <= 1:
 			self.move_stiffness = stiffness
+			return True
+		return False
 
 	def setRestStiffness(self, stiffness):
 		if stiffness >= 0 and stiffness <= 1:
 			self.rest_stiffness = stiffness
+			return True
+		return False
 
 	def setJointSpeed(self, speed):
 		if speed >= 0 and speed <= 1:
 			self.joint_speed = speed
+			return True
+		return False
