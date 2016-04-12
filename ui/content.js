2184c40580311271cb28b8c926c51e084db42c66
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2016-04-12 11:02:37 -0400

    late commit

--- ui/content.js
+++ ui/content.js
@@ -5,10 +5,15 @@ console.log(labels);
 $('#modes').append('<p class="html-variable" id="current-mode"></p>');
 for( var l in labels ) {
   console.log(labels[l]);
-  $('#tab-labels').append('<a class="tab-label" href="set_mode('+labels[l]+')">'+labels[l]+'</a>');
+  $('#tab-labels').append('<a class="tab-label" data-mode="'+labels[l]+'">'+labels[l]+'</a>');
   $('<article class="mode" id="'+labels[l]+'-Mode"><h2>'+labels[l]+' Mode</h2></article>').insertBefore('#mode-insertion-point');
 }
-set_mode('Demo');
+set_mode('Exercise');
+$('#tab-labels .tab-label').each(function () {
+  $(this).click(function() {
+    set_mode($(this).attr('data-mode'));
+  });
+});
 
 var joints = {
   headp: 'Head Pitch', heady: 'Head Yaw',
@@ -25,11 +30,11 @@ for( var j in joints ) {
   console.log(joint);
   $('#nao-joints-show').append('<label class="show-slide-label" for="'+j+'-show">'+joint+'</label>');
   $('#nao-joints-show').append('<br/>');
-  $('#nao-joints-show').append('<input type="range" class="show-slide" name="'+j+'-show" id="'+j+'-show" value="50" min="0" max="100">');
+  $('#nao-joints-show').append('<input type="range" class="show-slide" name="'+j+'-show" id="'+j+'-show" value="'+Math.floor(Math.random() * 101)+'" min="0" max="100">');
   $('#nao-joints-show').append('<br/>');
   $('#nao-joints-edit').append('<label class="edit-slide-label" for="'+j+'-show">'+joint+'</label>');
   $('#nao-joints-edit').append('<br/>');
-  $('#nao-joints-edit').append('<input type="range" class="edit-slide" name="'+j+'-edit" id="'+j+'-edit" value="50" min="0" max="100">');
+  $('#nao-joints-edit').append('<input type="range" class="edit-slide" name="'+j+'-edit" id="'+j+'-edit" value="'+Math.floor(Math.random() * 101)+'" min="0" max="100">');
   $('#nao-joints-edit').append('<br/>');
 }
 
@@ -44,6 +49,7 @@ function set_mode(mode) {
   }
   else if( mode == 'Assignment' ) {
   }
+  $('.mode').css('display','none');
   $('#'+mode+'-Mode').css('display','block');
   $('#current-mode').text(mode);
   if( mode == 'Demo' ) {
