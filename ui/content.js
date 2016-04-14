3db39aa895eb2869608d925f01fc438a1936c64b
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2016-04-14 16:22:32 -0400

    added three sections. demo is finished. entire ui is nearly finished. all that remains are script views. next is to add socket functionality

--- ui/content.js
+++ ui/content.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
 
-var labels = ['Demo','Exercise','Assignment'];
-console.log(labels);
+var modes = ['Demo','Exercise','Assignment'];
+console.log(modes);
 $('#modes').attr('data-mode','None');
-for( var l in labels ) {
-  console.log(labels[l]);
+for( var l in modes ) {
+  console.log(modes[l]);
   //$('#tab-labels').append('<a class="tab-label" data-mode="'+labels[l]+'">'+labels[l]+'</a>');
-  $('<article class="mode" id="'+labels[l]+'-Mode" data-mode="'+labels[l]+'"><div class="title-bar"><div class="tab-label" data-mode="'+labels[l]+'" data-mode-action="set"><h2>'+labels[l]+' Mode</h2></div><img class="mode-exit" data-mode="'+labels[l]+'" src="exit.gif"></div><main class="mode-main"></main></article>').insertBefore('#mode-insertion-point');
+  $('<article class="mode" id="'+modes[l]+'-Mode" data-mode="'+modes[l]+'"><heading class="title-bar"><div class="tab-label" data-mode="'+modes[l]+'" data-mode-action="set"><h2>'+modes[l]+' Mode</h2></div><img class="mode-exit" data-mode="'+modes[l]+'" src="back.png"></heading><div class="mode-container"><main class="mode-main"></main></div></article>').insertBefore('#mode-insertion-point');
 }
 $('.mode .tab-label').each(function () {
   $(this).click(function() {
     if( $(this).attr('data-mode-action') == 'set' ) {
-      set_mode($(this).attr('data-mode'));
+      //set_mode($(this).attr('data-mode'));
       $(this).attr('data-mode-action','none');
+      window.location.hash = '#' + $(this).attr('data-mode');
     }
     else if( $(this).attr('data-mode-action') == 'unset' ) {
       set_mode('None');
@@ -21,7 +22,8 @@ $('.mode .tab-label').each(function () {
   });
 });
 $('.mode-exit').click(function() {
-  set_mode('None');
+  //set_mode('None');
+  window.location.hash = '#None';
   $('.tab-label[data-mode='+$(this).attr('data-mode')+']').attr('data-mode-action','set');
 });
 
@@ -44,7 +46,7 @@ for( var j in joints ) {
   console.log(joint);
   $('#nao-joints-show').append('<label class="show-slide-label" for="'+j+'-show">'+joint+'</label>');
   $('#nao-joints-show').append('<br/>');
-  $('#nao-joints-show').append('<input type="range" data-joint-name="'+j+'" class="show-slide" name="'+j+'-show" id="'+j+'-show" value="'+Math.floor(Math.random() * 101)+'" min="0" max="100">');
+  $('#nao-joints-show').append('<input type="range" data-joint-name="'+j+'" class="show-slide" name="'+j+'-show" id="'+j+'-show" value="'+Math.floor(Math.random() * 101)+'" min="0" max="100" readonly>');
   $('#nao-joints-show').append('<br/>');
   $('#nao-joints-edit').append('<label class="edit-slide-label" for="'+j+'-show">'+joint+'</label>');
   $('#nao-joints-edit').append('<br/>');
@@ -52,6 +54,14 @@ for( var j in joints ) {
   $('#nao-joints-edit').append('<br/>');
 }
 
+$('.show-slide').each(function() {
+  $(this).mouseenter(function() {
+    $('#nao-diagram-img').attr('src','nao_joints_'+$(this).attr('data-joint-name')+'.jpg');
+  });
+  $(this).mouseleave(function() {
+    $('#nao-diagram-img').attr('src','nao_joints_none.jpg');
+  });
+});
 $('.edit-slide').each(function() {
   $(this).mouseenter(function() {
     $('#nao-diagram-img').attr('src','nao_joints_'+$(this).attr('data-joint-name')+'.jpg');
@@ -60,6 +70,15 @@ $('.edit-slide').each(function() {
     $('#nao-diagram-img').attr('src','nao_joints_none.jpg');
   });
 });
+
+$(window).hashchange(function() {
+  var mode = window.location.hash.substr(1);
+  if( mode == "" )
+    window.location.hash = '#None'
+  else
+    set_mode(mode);
+});
+$(window).hashchange();
 
 });
 
@@ -93,7 +112,10 @@ function set_mode(mode) {
         'margin': '0'
       },{
         duration: dur,
-        queue: false
+        queue: false,
+        complete: function() {
+          $('#'+mode+'-Mode .mode-main').css('display','block');
+        }
       });
       $('#'+mode+'-Mode .mode-exit').fadeIn({
         duration: dur,
@@ -145,7 +167,10 @@ function set_mode(mode) {
         'margin': '.5in',
       },{
         duration: dur,
-        queue: false
+        queue: false,
+        start: function() {
+          $('#'+p_mode+'-Mode .mode-main').css('display','none');
+        }
       });
       $('#'+p_mode+'-Mode .mode-exit').fadeOut({
         duration: dur,
@@ -171,17 +196,57 @@ function set_mode(mode) {
     else {
     }
     $('#modes').attr('data-mode',mode);
+    if( mode == 'Exercise' )
+      $('#nao-joints-edit').css('display','block');
+    else
+      $('#nao-joints-edit').css('display','none');
   }
 }
 
 function fillDemo() {
   var mode = $('#Demo-Mode');
+  var main = $('#Demo-Mode .mode-main');
+  main.append('<p>In Demo mode, you can select one one the demos below to run on the NAO. Status messages we be displayed in the Execution section and the current position of all the joints are shown in the sliders to the left.</p>');
+  addSelection(main,'Demo','Demos');
+  main.append('<div class="spacer"></div>');
+  addExecution(main,'Demo');
 }
 
 function fillExercise() {
   var mode = $('#Exercise-Mode');
+  var main = $('#Exercise-Mode .mode-main');
+  main.append('<p>In Exercise mode, you can enter commands to control the NAO. Status messages we be displayed in the Execution section and the current position of all the joints are shown in the sliders to the left.  You can also control the joints with the white sliders.</p>');
+  addEntry(main,'Exercise');
+  main.append('<div class="spacer"></div>');
+  addExecution(main,'Exercise');
 }
 
 function fillAssignment() {
   var mode = $('#Assignment-Mode');
+  var main = $('#Assignment-Mode .mode-main');
+  addSelection(main,'Assignment','Scripts');
+  main.append('<div class="spacer"></div>');
+  addExecution(main,'Assignment');
+}
+
+function addSelection(main,mode,types) {
+  selection = $('<section id="'+mode+'-Selection"></section>');
+  selection.append('<h2>Available '+types+'</h2>');
+  main.append(selection);
+}
+
+function addExecution(main,mode) {
+  execution = $('<section id="'+mode+'-Execution"></section>');
+  execution.append('<h2>Execution</h2>');
+  execution.append('<code class="execution-text" id="'+mode+'-Execution-Text">&gt;&gt;</code>');
+  main.append(execution);
+}
+
+function addEntry(main,mode) {
+  entry = $('<section id="'+mode+'-Entry"></section>');
+  entry.append('<h2>Command Prompt</h2>');
+  prompt = $('<form></form>');
+  prompt.append('<input class="entry-prompt" style="width: 100%;" id="'+mode+'-Entry-Prompt" type="text"/>');
+  entry.append(prompt);
+  main.append(entry);
 }
